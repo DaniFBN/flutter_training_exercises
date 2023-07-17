21d4b1e40284002f355679252c5d8b2f67bc6bbb
DaniFBN <Dani_FBN@outlook.com.br>
2023-07-17 17:25:28 -0300

    linter

--- linter/response/lib/my_home_page.dart
+++ linter/response/lib/my_home_page.dart
@@ -1,34 +1,17 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
+  // Pode ser fixado comentado o porquê foi ignorado ou tirando o ignore.
+  // Nesse caso, a ideia era comentar o porquê
   // ignore: use_super_parameters
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  final dynamic title;
+  final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -39,28 +22,40 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
 
-    print(pi);
+    debugPrint(pi.toString());
 
-    valueNotifier.addListener(() {
-      print(valueNotifier.value);
+    valueNotifier.addListener(listener);
+  }
+
+  void onPressed() {
+    setState(() {
+      _counter++;
     });
+  }
+
+  void listener() {
+    print(valueNotifier.value);
+  }
+
+  @override
+  void dispose() {
+    valueNotifier.removeListener(listener);
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: theme.colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.only(
-            bottom: 10,
-            top: 10,
-            right: 20,
-            left: 20,
-          ),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
@@ -69,13 +64,13 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Text(
                 '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
+                style: theme.textTheme.headlineMedium,
               ),
-              Expanded(
+              const Expanded(
                 child: SizedBox(),
               ),
               ElevatedButton(
-                onPressed: () => throw 20,
+                onPressed: () => throw Exception(),
                 child: const Text('throw Exception'),
               ),
             ],
@@ -83,11 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _counter++;
-          });
-        },
+        onPressed: onPressed,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),

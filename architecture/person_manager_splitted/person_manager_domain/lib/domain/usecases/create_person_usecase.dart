72a6d7c3fe8ac80c4be26f694e6170eae222fd20
DaniFBN <Dani_FBN@outlook.com.br>
2023-09-25 13:55:47 -0300

    person manager domain layer

---
+++ architecture/person_manager_splitted/person_manager_domain/lib/domain/usecases/create_person_usecase.dart
@@ -0,0 +1,55 @@
+import '../entities/person_entity.dart';
+import '../params/create_person_param.dart';
+import '../repositories/i_person_repository.dart';
+
+abstract class ICreatePersonUsecase {
+  Future<PersonEntity> call(CreatePersonParam param);
+}
+
+class CreatePersonUsecase implements ICreatePersonUsecase {
+  final IPersonRepository _repository;
+
+  const CreatePersonUsecase(this._repository);
+
+  @override
+  Future<PersonEntity> call(CreatePersonParam param) {
+    final handledName = param.name.trim();
+    final nameRegex = RegExp('[0-9]');
+    if (nameRegex.hasMatch(handledName) ||
+        handledName.length <= 1 ||
+        !handledName.contains(' ')) {
+      throw Exception('Nome inválido');
+    }
+
+    if (!param.email.contains('@')) {
+      throw Exception('Email inválido');
+    }
+
+    final handledCpf = param.cpf.trim().replaceAll('[.-]', '');
+    final cpfRegex = RegExp(r'^[0-9]{11}$');
+    if (!cpfRegex.hasMatch(handledCpf)) {
+      throw Exception('CPF inválido');
+    }
+
+    final now = DateTime.now();
+    final ago130Years = now.subtract(const Duration(days: 47450));
+    if (param.birthAt.isBefore(ago130Years)) {
+      throw Exception('Data de nascimento inválida. Maior de 130');
+    }
+
+    final handledPhone = param.phone.trim().replaceAll('[()- ]', '');
+    final phoneRegex = RegExp(r'^[0-9]{11}$');
+    if (!phoneRegex.hasMatch(handledPhone)) {
+      throw Exception('Telefone inválido');
+    }
+
+    if (param.address.street.isEmpty ||
+        param.address.city.isEmpty ||
+        param.address.state.isEmpty ||
+        param.address.number.isEmpty) {
+      throw Exception('Endereço inválido');
+    }
+
+    return _repository.create(param);
+  }
+}
